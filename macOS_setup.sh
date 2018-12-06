aa1a609d83eba08d4470428eb2d2f54b174be589
Erik Swanson <erik.swanson@seattlechildrens.org>
2018-12-06 15:04:33 -0800

    added pause on install & moved teams install

--- macOS_setup.sh
+++ macOS_setup.sh
@@ -5,15 +5,16 @@ xcode-select --install
 
 echo "Installing HomeBrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "Running 'brew doctor'"
-brew doctor
+read -p "Press [Enter] key after Xcode installation completes"
 
 echo "Installing Brew formulae apps"
 brew install r csvkit nmap python dos2unix netcat git gpg ical-buddy tmux wget
 
 echo "Installing Brew cask apps"
-brew cask install skype firefox keepassx slack wireshark microsoft-teams google-chrome
+brew cask install skype firefox keepassx slack wireshark google-chrome
+
+echo "Installing microsoft-teams with Brew cask"
+brew cask install microsoft-teams 
 
 echo "Creating directory structure"
 mkdir -p $HOME/src/github/swansun/{priv,pub}
