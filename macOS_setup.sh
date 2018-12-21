63ca93afcdddacb324353a719ccea3a72643ee28
Erik Swanson <erik.swanson@seattlechildrens.org>
2018-12-21 14:09:21 -0800

    added virtualbox and vagrant

--- macOS_setup.sh
+++ macOS_setup.sh
@@ -8,10 +8,10 @@ echo "Installing HomeBrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Installing Brew formulae apps"
-brew install r csvkit nmap python dos2unix netcat git gpg ical-buddy tmux wget mas lastpass-cli bash-completion
+brew install r csvkit nmap python dos2unix netcat git gpg ical-buddy tmux wget mas lastpass-cli bash-completion vagrant-completion
 
 echo "Installing Brew cask apps"
-brew cask install skype firefox keepassx slack wireshark google-chrome iterm2
+brew cask install skype firefox keepassx slack wireshark google-chrome iterm2 virtualbox vagrant
 
 echo "Installing microsoft-teams with Brew cask"
 brew cask install microsoft-teams 
