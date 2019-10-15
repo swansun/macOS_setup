9d10fabb9441985e04106dec4bdb2f6b2a987f11
Erik Swanson <erik.swanson@seattlechildrens.org>
2019-10-15 13:10:41 -0700

    +docker

--- macOS_setup.sh
+++ macOS_setup.sh
@@ -11,10 +11,10 @@ echo "Installing Brew formulae apps"
 brew install r csvkit nmap python dos2unix netcat git gpg ical-buddy tmux wget mas lastpass-cli bash-completion vagrant-completion ansible tree jq r ansible-lint
 
 echo "Installing Brew cask apps"
-brew cask install firefox keepassx slack wireshark google-chrome iterm2 virtualbox vagrant xquartz powershell
+brew cask install firefox keepassx slack wireshark google-chrome iterm2 virtualbox vagrant xquartz powershell docker
 
 echo "Installing microsoft-teams with Brew cask"
-brew cask install microsoft-teams 
+brew cask install microsoft-teams
 
 echo "Open the 'App Store' and sign in with Apple account"
 read -p "Press [Enter] key after singing in to App Store"
